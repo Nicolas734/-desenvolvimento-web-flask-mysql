1d30dddbab9e34e20999e156e15226b90b0520cd
Nicolas Lima <nichollaslimma734@gmail.com>
2021-12-17 21:16:36 -0300

    inplementação da base de dados

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+import mysql.connector
 
-app = Flask("__name__")
+app = Flask(__name__)
+
+connection = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="fatec2021",
+  database="unes"
+)
+mysql = MySQL(app)
+
 
 @app.route("/")
-def hellow_wolrd():
-    return "<h1>Judy Alvarez</h1>"
+def Home():
+    return render_template("Home.html")
 
 @app.route("/Home/")
-def Home():
+def Home3():
     return render_template("Home.html")
 
 @app.route("/Quem_Somos/")
@@ -20,12 +31,39 @@ def Contato():
 
 @app.route("/Quem_Somos")
 def Quem_Somos2():
-    return render_template("Quem_Somos.html");
+    return render_template("Quem_Somos.html")
 
 @app.route("/Contato")
 def Contato2():
-    return render_template("Contato.html");
+    return render_template("Contato.html")
 
 @app.route("/Home")
 def Home2():
     return render_template("Home.html")
+
+
+
+@app.route("/enviar",methods=["POST"])
+def post_arquivo():
+    if request.method == 'POST':
+        cursor = connection.cursor()
+        email = request.form['email']
+        assunto = request.form['assunto']
+        descricao = request.form['descricao']
+        querry = 'INSERT INTO usuario (email, assunto, descricao) VALUES (%s, %s, %s)'
+        insert = (email, assunto, descricao)
+        
+        if not request.form['email']:
+            return render_template("Contato.html")
+        else:
+            cursor.execute(querry, insert) 
+            connection.commit()
+
+            cursor.close()
+            connection.close()
+        return redirect(url_for('Contato'))
+
+
+
+if __name__=="__main__":
+    app.run(debug=True,port=5000)
